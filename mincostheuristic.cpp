6f581be37bc26914b243214adce797234564508c
Didanny <daa50@mail.aub.edu>
2020-12-15 01:04:21 -0800

    Complete minimum cost matching heuristic

--- mincostheuristic.cpp
+++ mincostheuristic.cpp
@@ -31,6 +31,7 @@ struct MinCostHeuristic: Heuristic
 	matrix<int> box_goal_adjacency;
 	std::map<int, int> goal_graph_to_adj_key;
 	std::map<int, int> box_graph_to_adj_key;
+	double cost;
 
     MinCostHeuristic() : Heuristic()
 	{
@@ -159,14 +160,14 @@ struct MinCostHeuristic: Heuristic
 				//Coord left = Coord(x-1, y);
 				//Coord right = Coord(x+1, y);	
 
-				if (y > 0 && y < game.board.dimensions.y - 1 && (game.board.get_field(Coord(x, y+1)) == 0 || game.board.get_field(Coord(x, y+1)) == 2 || game.board.get_field(Coord(x, y+1)) == 4) && (game.board.get_field(Coord(x, y-1)) == 0 || game.board.get_field(Coord(x, y-1)) == 2 || game.board.get_field(Coord(x, y-1)) == 4))
+				if (y > 0 && y < game.board.dimensions.y - 1 && (game.board.get_field(Coord(x, y+1)) == 0 /*|| game.board.get_field(Coord(x, y+1)) == 2*/ || game.board.get_field(Coord(x, y+1)) == 4) && (game.board.get_field(Coord(x, y-1)) == 0 || game.board.get_field(Coord(x, y-1)) == 2 || game.board.get_field(Coord(x, y-1)) == 4))
 				{
 					//std::cout << "First\n";
 					reverse_directed_graph(coord_to_key.at(Coord(x, y+1)), e.first) = 1;
 					//std::cout << "Second\n";
 					reverse_directed_graph(coord_to_key.at(Coord(x, y-1)), e.first) = 1;
 				}
-				if (x > 0 && x < game.board.dimensions.x - 1 && (game.board.get_field(Coord(x+1, y)) == 0 || game.board.get_field(Coord(x+1, y)) == 2 || game.board.get_field(Coord(x+1, y)) == 4) && (game.board.get_field(Coord(x-1, y)) == 0 || game.board.get_field(Coord(x-1, y)) == 2 || game.board.get_field(Coord(x-1, y)) == 4))
+				if (x > 0 && x < game.board.dimensions.x - 1 && (game.board.get_field(Coord(x+1, y)) == 0 /*|| game.board.get_field(Coord(x+1, y)) == 2*/ || game.board.get_field(Coord(x+1, y)) == 4) && (game.board.get_field(Coord(x-1, y)) == 0 || game.board.get_field(Coord(x-1, y)) == 2 || game.board.get_field(Coord(x-1, y)) == 4))
 				{
 					//std::cout << "Third\n";
 					reverse_directed_graph(coord_to_key.at(Coord(x+1, y)), e.first) = 1;
@@ -338,7 +339,7 @@ struct MinCostHeuristic: Heuristic
 				}				
 			}			
 		}
-		display_box_goal_adjacency();
+		//display_box_goal_adjacency();
 	}
 
 	void minimum_cost()
@@ -357,8 +358,9 @@ struct MinCostHeuristic: Heuristic
 		std::vector<int> assignment;
 		HungarianAlgorithm hungarian;
 
-		double cost = hungarian.Solve(cost_matrix, assignment);
-		std::cout << "Cost: " << cost << '\n';
+		cost = hungarian.Solve(cost_matrix, assignment);
+
+		//std::cout << "Cost: " << cost << '\n';
 	}
 
 	double operator()(State &state) {
@@ -380,39 +382,30 @@ struct MinCostHeuristic: Heuristic
 			build_box_goal_adjacency(game);
 			//std::cout << "Built box_goal_adjacency\n";
 			minimum_cost();
-			std::cout << board_to_string(game);
-			return +INFINITY;
 		}
-		throw "STOP";
-		double player_to_box = +INFINITY;
-		double box_to_goal = +INFINITY;
-		for(int x0 = 0; x0 < game.board.dimensions.x; x0++) {
-			for(int y0 = 0; y0 < game.board.dimensions.y; y0++) {
-				Coord pos1 = {x0, y0};
-				Board::Field field1 = game.board.get_field(pos1);
-				if(field1 != Board::box) {
-					continue;
-				}
-				for(int x1 = 0; x1 < game.board.dimensions.x; x1++) {
-					for(int y1 = 0; y1 < game.board.dimensions.y; y1++) {
-						Coord pos2 = {x1, y1};
-						if(pos1 == pos2) {
-							continue;
-						}
-						Board::Field field2 = game.board.get_field(pos2);
-						double d = std::abs(pos1.x-pos2.x)
-							   + std::abs(pos1.y-pos2.y);
-						if(pos2 == game.player) {
-							player_to_box = std::min(player_to_box, d);
-						}
-						if(field2 == Board::goal) {
-							box_to_goal = std::min(box_to_goal, d);
-						}
-					}
-				}
+		else
+		{
+			if (!update_box_keys(game))
+			{
+
+			}
+			else 
+			{
+				build_reverse_directed_graph(game);
+				build_distances_to_goals();
+				build_box_goal_adjacency(game);
+				minimum_cost();
 			}
 		}
-		return player_to_box + box_to_goal;
+		if (cost > 2147483646)
+		{
+			return +INFINITY;
+		}
+		else
+		{
+			return cost;
+		}
+		
 	}
 };
 
